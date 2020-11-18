5022813ba1895022d4de9e7a13584d43edf9e896
Mukul Sharma <msharma@3222-mbp15.attlocal.net>
2020-11-17 22:00:14 -0800

    Updates

---
+++ precache-manifest.593294bc2174d7cc5bcab779b989a888.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156552bd1e395b2a288d0023319bdd13",
+    "url": "/unit20-react/index.html"
+  },
+  {
+    "revision": "95034438a01d0d790eed",
+    "url": "/unit20-react/static/css/main.733a6162.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/unit20-react/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/unit20-react/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95034438a01d0d790eed",
+    "url": "/unit20-react/static/js/main.e748adb0.chunk.js"
+  },
+  {
+    "revision": "5fbffe2a3a5ed54efc41",
+    "url": "/unit20-react/static/js/runtime-main.727a88f0.js"
+  },
+  {
+    "revision": "add44b369f226545519dadb802eaaa40",
+    "url": "/unit20-react/static/media/Profile.add44b36.pdf"
+  },
+  {
+    "revision": "46186d2b95f58fcdfb3ee7a31a51fed2",
+    "url": "/unit20-react/static/media/autoremind.46186d2b.jpg"
+  },
+  {
+    "revision": "eed70e54ca989bca2719b69529fe9ef0",
+    "url": "/unit20-react/static/media/book-search.eed70e54.jpg"
+  },
+  {
+    "revision": "c4ecc82ab4577128bc7c7859d58b0a69",
+    "url": "/unit20-react/static/media/budget-tracker.c4ecc82a.jpg"
+  },
+  {
+    "revision": "b51bd024965363f743bb849edeff5562",
+    "url": "/unit20-react/static/media/cover-image.b51bd024.jpg"
+  },
+  {
+    "revision": "1801b24a8c095bb6ed9256673c3883ee",
+    "url": "/unit20-react/static/media/note-taker.1801b24a.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/unit20-react/static/media/run-buddy.938f8fe0.jpg"
+  }
+]);
